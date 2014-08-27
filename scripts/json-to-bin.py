f852a68bd1ee31e44efb1713a9ef7fce66adb562
Egil Moeller <egil.moller@freecode.no>
2014-08-27 11:08:37 +0200

    Fixed unit tests

--- scripts/json-to-bin.py
+++ scripts/json-to-bin.py
@@ -4,6 +4,7 @@ import json
 import struct
 import datetime
 import dateutil.parser
+import typedmatrix.TypedMatrix
 
 if len(sys.argv) < 3:
     print """Usage:
@@ -34,27 +35,6 @@ with open(datafile) as f:
 def timestamp(data):
     return int(dateutil.parser.parse(data).strftime("%s"))
 
-def conv(data, t, default):
-    try:
-        return t(data)
-    except:
-        return default
-
-typemap = {
-    timestamp: 'Int32',
-    int: 'Int32',
-    float: 'Float32',
-    }
-
-typeformatmap = {
-    'Int32': 'i',
-    'Float32': 'f'
-    }
-typedefaultmap = {
-    'Int32': 0,
-    'Float32': 0.0
-    }
-
 print "Calculating header values..."
 
 datalen = len(data)
@@ -78,39 +58,19 @@ for i, d in enumerate(data):
             except:
                 continue
         if key not in cols:
-            cols[key] = {'name': key, 'type': typemap[t], 'min': value, 'max': value}
+            cols[key] = {'name': key, 'type': typedmatrix.TypedMatrix.typemap[t], 'min': value, 'max': value}
             coltypes[key] = t
         cols[key]['max'] = max(cols[key]['max'], value)
         cols[key]['min'] = min(cols[key]['min'], value)
     if i % 1000 == 0:
         print "%.2f%%" % (100 * float(i) / datalen)
 
-if 'colsByName' in header:
-    colsByName = header.pop("colsByName")
-    for key in cols:
-        if key in colsByName:
-            cols[key].update(colsByName[key])
-
 cols = cols.values()
 cols.sort(lambda a, b: cmp(a['name'], b['name']))
-header.update({'cols': cols, 'length': len(data), 'series': nrseries})
+header.update({'length': len(data), 'series': nrseries})
 
 print "Header: ", header
-headerstr = json.dumps(header)
-print "Header length: ", len(headerstr)
-
 print "Writing data..."
 
 with open(outfile, "w") as f:
-    f.write('tmtx');
-    f.write(struct.pack("<i", len(headerstr)))
-    f.write(headerstr)
-
-    formatmap = '<' + ''.join(typeformatmap[col['type']] for col in cols)
-    colspecs = [{'name': col['name'], 'type': coltypes[col['name']], 'default': typedefaultmap[col['type']]} for col in cols]
-
-    for d in data:
-        f.write(struct.pack(
-                formatmap,
-                *[conv(d[colspec['name']], colspec['type'], colspec['default'])
-                  for colspec in colspecs]))
+    f.write(typedmatrix.TypedMatrix.pack(data, header, cols))
