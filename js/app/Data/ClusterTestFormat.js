73fd0969ca663d43ad365755ce1b14b042ab0d24
Egil Moeller <egil.moller@freecode.no>
2014-10-02 17:11:41 +0200

    Exp scale

--- js/app/Data/ClusterTestFormat.js
+++ js/app/Data/ClusterTestFormat.js
@@ -13,16 +13,16 @@ define(["app/Class", "app/Events", "app/Data/Format", "app/Data/EmptyFormat"], f
       var start = new Date("1970-01-01T00:00:00").getTime();
       var end = new Date("1970-01-20T00:00:00").getTime();
 
-      var rows = 20;
+      var rows = 10;
       var radiuses = [0.25, 0.5, 1.0, 1.5, 2.0];
 
-      self.rowcount = (rows + rows * rows / 2) * radiuses.length;
+      self.rowcount = (rows + rows * Math.pow(2, rows)) * radiuses.length;
       self.header = {
           "tilesetName": "Clustering Test",
           "colsByName": {
             "series":    {"max": 1.0, "min": 0.0, "type": "Float32"},
             "datetime":  {"max": end, "min": start, "type": "Float32"},
-            "weight":    {"max": 10 * (rows+1), "min": 0.0, "type": "Float32"},
+            "weight":    {"max": 10 * Math.pow(2, rows), "min": 0.0, "type": "Float32"},
             "sigma":     {"max": 1.0, "min": 0.0, "type": "Float32"},
 
             "latitude":  {"max": 90.0, "min": 0, "type": "Float32"},
@@ -45,10 +45,16 @@ define(["app/Class", "app/Events", "app/Data/Format", "app/Data/EmptyFormat"], f
 
       radiuses.map(function (radius) {
         for (var row = 0; row < rows; row++) {
+          var points = Math.pow(2, row);
+
           self.data.series[i] = 0;
           self.data.datetime[i] = row * (end - start) / rows;
-          self.data.weight[i] = 10 * (row + 1);
-          self.data.sigma[i] = radius;
+          self.data.weight[i] = 10 * points;
+          if (points > 1) {
+            self.data.sigma[i] = radius;
+          } else {
+            self.data.sigma[i] = 0;
+          }
           self.data.latitude[i] = row * 90 / rows;
           self.data.longitude[i] = displacement;
           self.data.rowidx[i] = i;
@@ -57,7 +63,8 @@ define(["app/Class", "app/Events", "app/Data/Format", "app/Data/EmptyFormat"], f
 
 
         for (var row = 0; row < rows; row++) {
-          for (var point = 0; point < row + 1; point++) {
+          var points = Math.pow(2, row);
+          for (var point = 0; point < points; point++) {
             self.data.series[i] = 0;
             self.data.datetime[i] = row * (end - start) / rows;
             self.data.weight[i] = 10;
@@ -67,7 +74,7 @@ define(["app/Class", "app/Events", "app/Data/Format", "app/Data/EmptyFormat"], f
             self.data.rowidx[i] = i;
 
             if (row > 0) {
-              var angle = 2 * Math.PI * point / (row + 1);
+              var angle = 2 * Math.PI * point / points;
               self.data.latitude[i] += Math.sin(angle) * radius;
               self.data.longitude[i] += Math.cos(angle) * radius;
             }
