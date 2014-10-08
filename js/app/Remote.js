8797dc4ff320e5730a42c437c3538eba7b4b351a
Egil Moeller <egil.moller@freecode.no>
2014-10-08 11:24:30 +0200

    Rate limiting

--- js/app/Remote.js
+++ js/app/Remote.js
@@ -17,7 +17,7 @@ define(["app/Class", "socketio"], function(Class, io) {
       self.socket.on('msg', function (data) {
         if (data.origin == self.clientId) return;
         if (data.context == 'visualization.state' && data.event == 'set') {
-          if (self.visualization.state.spec[data.name].fromurl) {
+          if (self.visualization.state.spec[data.name] && self.visualization.state.spec[data.name].fromurl) {
             data.value = self.visualization.state.spec[data.name].fromurl(data.value);
           }
           self.inUpdate = true;
@@ -28,19 +28,29 @@ define(["app/Class", "socketio"], function(Class, io) {
         }
       });
 
+      self.visualization_state_updates = {};
+
       self.visualization.state.events.on({
         set: function (args) {
           if (self.inUpdate) return;
+          is_new = !self.visualization_state_updates[args.name];
+
           var e = {
             context:'visualization.state',
             event:'set',
             name: args.name,
             value: args.new_value
           };
-          if (self.visualization.state.spec[e.name].tourl) {
+          if (self.visualization.state.spec[e.name] && self.visualization.state.spec[e.name].tourl) {
             e.value = self.visualization.state.spec[args.name].tourl(e.value)
           }
-          self.socket.emit("msg", e);
+          self.visualization_state_updates[args.name] = e;
+          if (is_new) {
+            setTimeout(function () {
+              self.socket.emit("msg", self.visualization_state_updates[args.name]);
+              delete self.visualization_state_updates[args.name];
+            }, 200);
+          }
         }
       });
 
