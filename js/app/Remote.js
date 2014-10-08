7d2cb3d0178b18a47654dbf7828830f7f5b45209
Egil Moeller <egil.moller@freecode.no>
2014-10-08 12:55:01 +0200

    Generalized the Remote code a bit

--- js/app/Remote.js
+++ js/app/Remote.js
@@ -1,4 +1,4 @@
-define(["app/Class", "socketio"], function(Class, io) {
+define(["app/Class", "socketio", "async"], function(Class, io, async) {
   return Class({
     name: "Remote",
     initialize: function (visualization) {
@@ -10,51 +10,88 @@ define(["app/Class", "socketio"], function(Class, io) {
       var self = this;
 
       self.inUpdate = false;
+
+
+      async.series([
+        self.initSocketIO.bind(self),
+        self.init_visualization_state.bind(self)
+      ], cb);
+    },
+
+    initSocketIO: function (cb) {
+      var self = this;
+
       self.socket = io.connect('http://localhost:4711');
       self.socket.on('id', function (data) {
         self.clientId = data.id;
       });
       self.socket.on('msg', function (data) {
         if (data.origin == self.clientId) return;
-        if (data.context == 'visualization.state' && data.event == 'set') {
-          if (self.visualization.state.spec[data.name] && self.visualization.state.spec[data.name].fromurl) {
-            data.value = self.visualization.state.spec[data.name].fromurl(data.value);
-          }
-          self.inUpdate = true;
-          self.visualization.state.setValue(data.name, data.value);
-          self.inUpdate = false;
+
+        var method = self['handle_' + data.event];
+        if (method) {
+          method.bind(self)(data);
         } else {
           console.log(["Unknown event", data]);
         }
       });
+      self.pendingUpdates = {};
+      cb();
+    },
+
+    addUpdate: function(event, name, value) {
+      var self = this;
+
+      if (self.inUpdate) return;
+
+      var key = event + "_" + name;
+      var is_new = !self.pendingUpdates[key];
+
+      var e = {
+        event: event,
+        name: name,
+        value: value
+      };
+      self.pendingUpdates[key] = e;
+      if (is_new) {
+        setTimeout(function () {
+          self.socket.emit("msg", self.pendingUpdates[key]);
+          delete self.pendingUpdates[key];
+        }, 200);
+      }
+    },
+
+    init_visualization_state: function (cb) {
+      var self = this;
 
       self.visualization_state_updates = {};
 
       self.visualization.state.events.on({
         set: function (args) {
-          if (self.inUpdate) return;
-          is_new = !self.visualization_state_updates[args.name];
+          var name = args.name;
+          var value = args.new_value;
+          var spec = self.visualization.state.spec[args.name];
 
-          var e = {
-            context:'visualization.state',
-            event:'set',
-            name: args.name,
-            value: args.new_value
-          };
-          if (self.visualization.state.spec[e.name] && self.visualization.state.spec[e.name].tourl) {
-            e.value = self.visualization.state.spec[args.name].tourl(e.value)
+          if (spec && spec.tourl) {
+            value = spec.tourl(value);
           }
-          self.visualization_state_updates[args.name] = e;
-          if (is_new) {
-            setTimeout(function () {
-              self.socket.emit("msg", self.visualization_state_updates[args.name]);
-              delete self.visualization_state_updates[args.name];
-            }, 200);
-          }
+
+          self.addUpdate('visualization_state_set', name, value);
         }
       });
 
       cb();
+    },
+
+    handle_visualization_state_set: function (data) {
+      var self = this;
+      if (self.visualization.state.spec[data.name] && self.visualization.state.spec[data.name].fromurl) {
+        data.value = self.visualization.state.spec[data.name].fromurl(data.value);
+      }
+      self.inUpdate = true;
+      self.visualization.state.setValue(data.name, data.value);
+      self.inUpdate = false;
     }
+
   });
 });
