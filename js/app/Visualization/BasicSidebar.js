c3c49ad0e2993931e5fd8703ae651397d258d59c
Johnna Armstrong <johnna@biztechsource.com>
2014-10-27 21:40:29 -0400

    Add div for Google logo and reposition logos

--- js/app/Visualization/BasicSidebar.js
+++ js/app/Visualization/BasicSidebar.js
@@ -25,7 +25,7 @@ if (app.useDojo) {
           '    <div id="drawer_slide"><img src="' + app.dirs.img + '/gfw/gfw_close.png"></div>' +
           '  ' +
           '    <div id="gfw_title"><img src="' + app.dirs.img + '/gfw/gfw_logo.png">Global Fishing Watch</div>' +
-          '    <div id="gfw_logos"><img class="oc" src="' + app.dirs.img + '/gfw/oceana_logo.png"><img class="st" src="' + app.dirs.img + '/gfw/st_logo.png"></div>' +
+          '    <div id="gfw_logos"><img class="st" src="' + app.dirs.img + '/gfw/st_logo.png"><img class="oc" src="' + app.dirs.img + '/gfw/oceana_logo.png"><img class="g" src="' + app.dirs.img + '/gfw/google_logo.png"></div>' +
           '' +
           '    <div id="divide"></div>' +
           '' +
