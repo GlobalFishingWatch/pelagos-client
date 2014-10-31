dedaa1a0e5ab89fb81af0abab7df842689d20b0b
Egil Moeller <egil.moller@freecode.no>
2014-10-31 12:13:16 +0100

    Better code structure

--- js/testgl.js
+++ js/testgl.js
@@ -1,176 +1,207 @@
-datasetsnr = 1;
-datasetpointsnr = 1000000;
-
-function addHeadScript(script, cb) {
-  if (typeof(script) == "string") script = {url: script};
-  var head = document.getElementsByTagName('head')[0];
-  var tag = document.createElement('script');
-  tag.type = script.type || 'text/javascript';
-  tag.src = script.url;
-  tag.onload = cb;
-  head.appendChild(tag);
-}
-
-shader_vertex_source = [
-  'attribute vec2 position;',
-  'attribute vec3 color;',
-  '',
-  'varying vec3 vColor;',
-  '',
-  'void main(void) {',
-  '  gl_Position = vec4(position, 0., 1.);',
-  '  gl_PointSize = 20.0;',
-  '  vColor=color;',
-  '}'
-].join("\n");
-
-shader_fragment_source = [
-  'precision mediump float;',
-  '',
-  'varying vec3 vColor;',
-  '',
-  'void main(void) {',
-  '  gl_FragColor = vec4(vColor, 1.);',
-  '}'
-].join("\n");
 
 
-addHeadScript("http://code.jquery.com/jquery-1.11.0.min.js", function () {
-  addHeadScript("http://code.jquery.com/jquery-migrate-1.2.1.min.js", function () {
- 
+function testGl(args, cb) {
+  if (!args.pointsPerDataset) args.pointsPerDataset = 1000000;
+  if (!args.datasets) args.datasets = 1;
+  if (!args.frames) args.frames = 100;
 
-    $(document).ready(function() {
-      $("body").append("<canvas id='your_canvas' style='position: absolute; background-color: black;'></canvas>");
-      $("body").append("<div class='test' style='position: absolute; bottom: 0; right: 0; background: red;'></div>");
 
-      var CANVAS=$("#your_canvas")[0];
-      CANVAS.width=window.innerWidth;
-      CANVAS.height=window.innerHeight;
+  var shader_vertex_source = [
+    'attribute vec2 position;',
+    'attribute vec3 color;',
+    '',
+    'varying vec3 vColor;',
+    '',
+    'void main(void) {',
+    '  gl_Position = vec4(position, 0., 1.);',
+    '  gl_PointSize = 10.0;',
+    '  vColor=color;',
+    '}'
+  ].join("\n");
 
-      var c = 0;
-      setInterval(function () {
-        c++;
-        $(".test").html(c.toString());
-      }, 100);
+  var shader_fragment_source = [
+    'precision mediump float;',
+    '',
+    'varying vec3 vColor;',
+    '',
+    'void main(void) {',
+    '  gl_FragColor = vec4(vColor, 1.);',
+    '}'
+  ].join("\n");
 
-      try {
-        var GL = CANVAS.getContext("experimental-webgl", {antialias: true});
-      } catch (e) {
-        alert("You are not webgl compatible :(") ;
-        return false;
-      } ;
+  function performGlTest(args, cb) {
+    var res = {errors: []};
 
-      function get_shader_program(GL) {
-        var get_shader=function(source, type, typeString) {
-          var shader = GL.createShader(type);
-          GL.shaderSource(shader, source);
-          GL.compileShader(shader);
-          if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
-            alert("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader));
-            return false;
-          }
-          return shader;
-        };
+    var canvas = $("<canvas style='position: absolute; background-color: black;'></canvas>");
+    $("body").append(canvas);
 
-        var shader_vertex=get_shader(shader_vertex_source, GL.VERTEX_SHADER, "VERTEX");
-        var shader_fragment=get_shader(shader_fragment_source, GL.FRAGMENT_SHADER, "FRAGMENT");
+    canvas = canvas[0];
+    canvas.width=window.innerWidth;
+    canvas.height=window.innerHeight;
 
-        var SHADER_PROGRAM=GL.createProgram();
-        GL.attachShader(SHADER_PROGRAM, shader_vertex);
-        GL.attachShader(SHADER_PROGRAM, shader_fragment);
+    try {
+      var GL = canvas.getContext("experimental-webgl", {antialias: true});
+    } catch (e) {
+      res.errors.push("Unable to enable context experimental-webgl");
+      cb(res); return;
+    } ;
 
-        GL.linkProgram(SHADER_PROGRAM);
-
-        return SHADER_PROGRAM;
-      }
-
-      var SHADER_PROGRAM = get_shader_program(GL);
-      var _color = GL.getAttribLocation(SHADER_PROGRAM, "color");
-      var _position = GL.getAttribLocation(SHADER_PROGRAM, "position");
-
-      GL.enableVertexAttribArray(_color);
-      GL.enableVertexAttribArray(_position);
-
-      GL.useProgram(SHADER_PROGRAM);
-
-      var datasets = [];
-
-      for (var i = 0; i < datasetsnr; i++) {
-        var dataset = {};
-        datasets.push(dataset);
-
-        dataset.points = new Float32Array(2 * datasetpointsnr);
-        dataset.color = new Float32Array(3 * datasetpointsnr);
-        for (var j = 0; j < datasetpointsnr; j++) {
-          dataset.points[j*2] = Math.random()-0.5;
-          dataset.points[j*2 + 1] = Math.random()-0.5;
-          dataset.color[j*3] = Math.random();
-          dataset.color[j*3+1] = Math.random();
-          dataset.color[j*3+2] = Math.random();
+    function get_shader_program(GL) {
+      var get_shader=function(source, type, typeString) {
+        var shader = GL.createShader(type);
+        GL.shaderSource(shader, source);
+        GL.compileShader(shader);
+        if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
+          res.errors.push("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader));
+          cb(res); return;
         }
-      }
-
-      var start = performance.now();
-      for (var i = 0; i < datasetsnr; i++) {
-        dataset = datasets[i];
-
-        dataset.pointBuffer= GL.createBuffer();
-        dataset.colorBuffer= GL.createBuffer();
-        GL.bindBuffer(GL.ARRAY_BUFFER, dataset.pointBuffer);
-        GL.bufferData(GL.ARRAY_BUFFER, dataset.points, GL.STATIC_DRAW);
-
-        GL.bindBuffer(GL.ARRAY_BUFFER, dataset.colorBuffer);
-        GL.bufferData(GL.ARRAY_BUFFER, dataset.color, GL.STATIC_DRAW);
-      }
-      var end = performance.now();
-
-      console.log("Load time for " + datasetsnr + " arrays of " + datasetpointsnr + " points = " + (end - start));
-
-
-
-      GL.clearColor(0.0, 0.0, 0.0, 0.0);
-      GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height);
-
-      timings = {
-        time: 0,
-        count: 0
+        return shader;
       };
 
-      function renderFrame() {
+      var shader_vertex=get_shader(shader_vertex_source, GL.VERTEX_SHADER, "VERTEX");
+      var shader_fragment=get_shader(shader_fragment_source, GL.FRAGMENT_SHADER, "FRAGMENT");
+      if (!shader_vertex || !shader_fragment) return;
 
-        var start = performance.now();
+      var program=GL.createProgram();
+      GL.attachShader(program, shader_vertex);
+      GL.attachShader(program, shader_fragment);
 
-        GL.clear(GL.COLOR_BUFFER_BIT);
-        for (var i = 0; i < datasetsnr; i++) {
-          var dataset = datasets[i];
+      GL.linkProgram(program);
 
-          GL.bindBuffer(GL.ARRAY_BUFFER, dataset.pointBuffer);
-          GL.enableVertexAttribArray(_position);
-          GL.vertexAttribPointer(_position, 2, GL.FLOAT, false, 0, 0);
-          GL.bindBuffer(GL.ARRAY_BUFFER, dataset.colorBuffer);
-          GL.enableVertexAttribArray(_color);
-          GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 0, 0);
+      return program;
+    }
 
-          GL.drawArrays(GL.POINTS, 0, datasetpointsnr);
-        }
-        GL.flush();
+    var program = get_shader_program(GL);
+    if (!program) return;
+    var _color = GL.getAttribLocation(program, "color");
+    var _position = GL.getAttribLocation(program, "position");
 
-        var end = performance.now();
+    GL.enableVertexAttribArray(_color);
+    GL.enableVertexAttribArray(_position);
 
-        timings.time += end - start;
-        timings.count++;
+    GL.useProgram(program);
 
-        if (timings.count % 100 == 0) {
-          console.log("Avg. JS draw time: " + (timings.time / timings.count));
-        }
+    var datasets = [];
 
-        window.requestAnimationFrame(renderFrame, CANVAS);
+    for (var i = 0; i < args.datasets; i++) {
+      var dataset = {};
+      datasets.push(dataset);
+
+      dataset.points = new Float32Array(2 * args.pointsPerDataset);
+      dataset.color = new Float32Array(3 * args.pointsPerDataset);
+      for (var j = 0; j < args.pointsPerDataset; j++) {
+        dataset.points[j*2] = Math.random()-0.5;
+        dataset.points[j*2 + 1] = Math.random()-0.5;
+        dataset.color[j*3] = Math.random();
+        dataset.color[j*3+1] = Math.random();
+        dataset.color[j*3+2] = Math.random();
       }
+    }
 
-      renderFrame();
+    var start = performance.now();
+    for (var i = 0; i < args.datasets; i++) {
+      dataset = datasets[i];
+
+      dataset.pointBuffer= GL.createBuffer();
+      dataset.colorBuffer= GL.createBuffer();
+      GL.bindBuffer(GL.ARRAY_BUFFER, dataset.pointBuffer);
+      GL.bufferData(GL.ARRAY_BUFFER, dataset.points, GL.STATIC_DRAW);
+
+      GL.bindBuffer(GL.ARRAY_BUFFER, dataset.colorBuffer);
+      GL.bufferData(GL.ARRAY_BUFFER, dataset.color, GL.STATIC_DRAW);
+    }
+    var end = performance.now();
+
+    res.load = end - start;
+
+
+    GL.clearColor(0.0, 0.0, 0.0, 0.0);
+    GL.viewport(0.0, 0.0, canvas.width, canvas.height);
+
+    timings = {
+      time: 0,
+      count: 0
+    };
+
+    function renderFrame() {
+
+      var start = performance.now();
+
+      GL.clear(GL.COLOR_BUFFER_BIT);
+      for (var i = 0; i < args.datasets; i++) {
+        var dataset = datasets[i];
+
+        GL.bindBuffer(GL.ARRAY_BUFFER, dataset.pointBuffer);
+        GL.enableVertexAttribArray(_position);
+        GL.vertexAttribPointer(_position, 2, GL.FLOAT, false, 0, 0);
+        GL.bindBuffer(GL.ARRAY_BUFFER, dataset.colorBuffer);
+        GL.enableVertexAttribArray(_color);
+        GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 0, 0);
+
+        GL.drawArrays(GL.POINTS, 0, args.pointsPerDataset);
+      }
+      GL.flush();
+
+      var end = performance.now();
+
+      var duration = end - start;
+      if (timings.min == undefined || timings.min > duration) timings.min = duration;
+      if (timings.max == undefined || timings.max < duration) timings.max = duration;
+      timings.time += duration;
+      timings.count++;
+      timings.avg = timings.time / timings.count;
+
+      if (timings.count >= args.frames) {
+        res.draw = timings;
+        cb(res);
+      } else {
+        window.requestAnimationFrame(renderFrame, canvas);
+      }
+    }
+
+    renderFrame();
+  };
+
+
+
+
+  var performGlTestOnPageLoad = function () {
+    $(document).ready(function() {
+      performGlTest(args, function (res) {
+        console.log(res);
+
+        cb(res);
+      });
     });
+  }
 
+  if (typeof($) == "undefined") {
+    var addHeadScript = function(script, cb) {
+      if (typeof(script) == "string") script = {url: script};
+      var head = document.getElementsByTagName('head')[0];
+      var tag = document.createElement('script');
+      tag.type = script.type || 'text/javascript';
+      tag.src = script.url;
+      tag.onload = cb;
+      head.appendChild(tag);
+    }
 
+    addHeadScript("http://code.jquery.com/jquery-1.11.0.min.js", function () {
+      addHeadScript("http://code.jquery.com/jquery-migrate-1.2.1.min.js", function () {
+        performGlTestOnPageLoad();
+      });
+    });
+  } else {
+    performGlTestOnPageLoad();
+  }
+}
 
+function testGlWidget(args) {
+  testGl(args, function (res) {
+    if (res.errors.length == 0) {
+      $(args.result).html("<h1 style='color:#00ff00'>Congratulations: Your browser supports WebGL</h1>");
+    } else {
+      $(args.result).html("<h1 style='color:#ff0000'>Unfourtunately, your browser does not support our animation:</h1><div>" + res.errors.join("</div><div>") + "</div>");
+    }
   });
-});
+}
