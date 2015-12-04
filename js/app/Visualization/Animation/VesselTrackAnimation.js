16015736d013c5aff42ee8e865f5a811198d3a95
Enrique Tuya <etuya@skytruth.org>
2015-12-04 10:28:02 -0300

    Change the color to lightblue to make it similar to old tracks.

--- js/app/Visualization/Animation/VesselTrackAnimation.js
+++ js/app/Visualization/Animation/VesselTrackAnimation.js
@@ -1,3 +1,4 @@
+
 define(["app/Class", "app/Visualization/Animation/Animation"], function(Class, Animation) {
   var VesselTrackAnimation = Class(Animation, {
     name: "VesselTrackAnimation",
@@ -15,9 +16,9 @@ define(["app/Class", "app/Visualization/Animation/Animation"], function(Class, A
     },
 
     uniforms: {
-      red: {value: 0.0, min:0.0, max: 1.0},
-      green: {value: 0.0, min:0.0, max: 1.0},
-      blue: {value: 0.7, min:0.0, max: 1.0},
+      red: {value: 0.4, min:0.0, max: 1.0},
+      green: {value: 0.6, min:0.0, max: 1.0},
+      blue: {value: 0.9, min:0.0, max: 1.0},
 
       focus_red: {value: 1.0, min:0.0, max: 1.0},
       focus_green: {value: 1.0, min:0.0, max: 1.0},
