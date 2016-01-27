025df56a76b4e188ecafb2e1e35630c34e4c648b
Egil <egil.moller@freecode.no>
2016-01-27 10:56:07 +0100

    Paging for search

--- js/app/Visualization/UI/Search.js
+++ js/app/Visualization/UI/Search.js
@@ -54,14 +54,14 @@ define([
       self.dialog.show();
     },
 
-    performSearch: function (query) {
+    performSearch: function (query, offset, limit) {
       var self = this;
       self.displaySearchDialog();
 
       $(self.dialog.containerNode).find('.search-loading').show();
 
       self.animationManager.search(
-        query,
+        query, offset, limit,
         self.displaySearchResults.bind(self)
       );
     },
@@ -76,7 +76,25 @@ define([
       } else if (res.length == 0) {
         results.html('<div class="no-results">No results found</div>');
       } else {
-        results.html('<table class="table result-table">' +
+        results.html("");
+
+        results.append("<div class='paging'><button class='prev'>Prev</button> <span class='start'></span>-<span class='end'></span> of <span class='total'></span> <button class='next'>Next</button></div>");
+
+        results.find(".start").html(res.offset);
+        results.find(".end").html(res.offset + res.entries.length);
+        results.find(".total").html(res.total);
+
+        if (res.offset <= 0) results.find(".prev").attr({disabled: 'disabled'});
+        if (res.offset + res.limit  >= res.total) results.find(".next").attr({disabled: 'disabled'});
+
+        results.find(".prev").click(function () {
+          self.performSearch(res.query, res.offset - res.limit, res.limit);
+        });
+        results.find(".next").click(function () {
+          self.performSearch(res.query, res.offset + res.limit, res.limit);
+        });
+
+        results.append('<table class="table result-table">' +
                      '  <tr>' +
                      '    <th class="name">Name</th>' +
                      '    <th class="imo">IMO</th>' +
@@ -84,7 +102,7 @@ define([
                      '    <th class="callsign">Callsign</th>' +
                      '  </tr>' +
                      '</table>');
-        res.map(function (info) {
+        res.entries.map(function (info) {
           var row = $('<tr><td><a class="vesselname"></a></td><td><a class="imo"></a></td><td><a class="mmsi"></a></td><td><a class="callsign"></a></td></tr>');
           row.find(".vesselname").html(info.vesselname);
           row.find(".imo").html(info.imo);
