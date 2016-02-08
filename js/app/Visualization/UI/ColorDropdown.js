8d2826a04e22fce34942a45c74c8505425ddb3cd
Egil <egil.moller@freecode.no>
2016-02-08 18:15:14 +0100

    First attempt at a color picker dropdown

---
+++ js/app/Visualization/UI/ColorDropdown.js
@@ -0,0 +1,37 @@
+define([
+  "dojo/dom-style",
+  "dijit/_WidgetBase",
+  "dijit/_TemplatedMixin",
+  "dijit/_WidgetsInTemplateMixin",
+  "dijit/_AttachMixin",
+  "dijit/popup",
+  "dojox/widget/ColorPicker",
+  "dijit/form/DropDownButton",
+  "dijit/TooltipDialog"
+], function(
+   domStyle,
+   _Widget,
+   _Templated,
+   _WidgetsInTemplateMixin,
+   _AttachMixin,
+   popup,
+   ColorPicker,
+   DropDownButton,
+   TooltipDialog
+){
+  return dojo.declare("ColorDropdown", [_Widget, _Templated, _WidgetsInTemplateMixin, _AttachMixin], {
+    title: 'Color',
+    templateString: '<div>' +
+                    '  <div data-dojo-type="dijit/form/DropDownButton" data-dojo-attach-point="button">' +
+                    '    <span>${title}</span>' +
+                    '    <div data-dojo-type="dijit/TooltipDialog" data-dojo-attach-point="dialog" >' +
+                    '      <div data-dojo-type="dojox/widget/ColorPicker" data-dojo-attach-point="picker" data-dojo-attach-event="change: change"></div>' +
+                    '    </div>' +
+                    '  </div>' +
+                    '</div>',
+    change: function (color) {
+      popup.close(this.dialog);
+      domStyle.set(this.button.containerNode, 'backgroundColor', color);
+    }
+  });
+});
