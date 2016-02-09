3fe5cf172032a963a2eb29eb16747a41b404876c
Egil <egil.moller@freecode.no>
2016-02-09 11:32:30 +0100

    ColorDropdown is now a _FormWidget

--- js/app/Visualization/UI/ColorDropdown.js
+++ js/app/Visualization/UI/ColorDropdown.js
@@ -1,37 +1,77 @@
 define([
   "dojo/dom-style",
-  "dijit/_WidgetBase",
-  "dijit/_TemplatedMixin",
-  "dijit/_WidgetsInTemplateMixin",
-  "dijit/_AttachMixin",
-  "dijit/popup",
   "dojox/widget/ColorPicker",
-  "dijit/form/DropDownButton",
-  "dijit/TooltipDialog"
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "dijit/form/RangeBoundTextBox",
+  "dijit/_HasDropDown",
+  "dijit/popup",
+  "dojo/text!dijit/form/templates/DropDownBox.html"
 ], function(
-   domStyle,
-   _Widget,
-   _Templated,
-   _WidgetsInTemplateMixin,
-   _AttachMixin,
-   popup,
-   ColorPicker,
-   DropDownButton,
-   TooltipDialog
+  domStyle,
+  ColorPicker,
+  declare,
+  lang,
+  RangeBoundTextBox,
+  _HasDropDown,
+    popup,
+  template
 ){
-  return dojo.declare("ColorDropdown", [_Widget, _Templated, _WidgetsInTemplateMixin, _AttachMixin], {
-    title: 'Color',
-    templateString: '<div>' +
-                    '  <div data-dojo-type="dijit/form/DropDownButton" data-dojo-attach-point="button">' +
-                    '    <span>${title}</span>' +
-                    '    <div data-dojo-type="dijit/layout/ContentPane" data-dojo-attach-point="dialog" style="background: white;">' +
-                    '      <div data-dojo-type="dojox/widget/ColorPicker" data-dojo-attach-point="picker" data-dojo-attach-event="change: change"></div>' +
-                    '    </div>' +
-                    '  </div>' +
-                    '</div>',
-    change: function (color) {
-      popup.close(this.dialog);
-      domStyle.set(this.button.containerNode, 'backgroundColor', color);
+
+  return declare("ColorDropdown", [RangeBoundTextBox, _HasDropDown], {
+    templateString: template,
+    hasDownArrow: true,
+    cssStateNodes: {
+      "_buttonNode": "dijitDownArrowButton"
+    },
+    dropDownDefaultValue: '#ffffff',
+    value: '#ffffff',
+    pattern: '#[0-9a-fA-F]{6}',
+
+    buildRendering: function(){
+      this.inherited(arguments);
+
+      if(!this.hasDownArrow){
+        this._buttonNode.style.display = "none";
+      }
+
+      if(!this.hasDownArrow){
+        this._buttonNode = this.domNode;
+        this.baseClass += " dijitComboBoxOpenOnClick";
+      }
+    },
+
+    _setValueAttr: function(value, priorityChange, formattedValue){
+      this.inherited(arguments, [value, priorityChange, formattedValue]);
+      if(this.dropDown){
+        this.dropDown.set('value', value, false);
+      }
+    },
+
+    openDropDown: function(callback){
+      if(this.dropDown){
+        this.dropDown.destroy();
+      }
+      var textBox = this,
+        value = this.get("value");
+      this.dropDown = new ColorPicker({
+        onChange: function(value){
+          textBox.set('value', value, true);
+          popup.close(textBox.dropDown);
+        },
+        style: 'background: white; padding: 10px;',
+        id: this.id + "_popup",
+        dir: textBox.dir,
+        lang: textBox.lang,
+        value: value,
+        textDir: textBox.textDir,
+      });
+
+      this.inherited(arguments);
+    },
+
+    onChange: function (value) {
+      domStyle.set(this._buttonNode, 'backgroundColor', value);
     }
   });
 });
