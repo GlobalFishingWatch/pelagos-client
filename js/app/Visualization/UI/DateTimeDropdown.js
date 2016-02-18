82519a5a605cd6932db8f94433dbfac4eef804b0
Egil <egil.moller@freecode.no>
2016-02-18 15:20:47 +0100

    New DateTimeDropdown widget

--- js/app/Visualization/UI/DateTimeDropdown.js
+++ js/app/Visualization/UI/DateTimeDropdown.js
@@ -2,7 +2,7 @@ define([
   "dojo/dom-style",
   "dojo/_base/declare",
   "dojo/_base/lang",
-  "dijit/form/RangeBoundTextBox",
+  "dijit/form/_DateTimeTextBox",
   "dijit/_HasDropDown",
   "dijit/_WidgetBase",
   "dijit/_TemplatedMixin",
@@ -16,7 +16,7 @@ define([
   domStyle,
   declare,
   lang,
-  RangeBoundTextBox,
+  _DateTimeTextBox,
   _HasDropDown,
   _WidgetBase,
   _TemplatedMixin,
@@ -25,78 +25,20 @@ define([
   popup,
   template
 ){
-
-  var DateTimeDropdown = declare("DateTimeDropdown", [RangeBoundTextBox, _HasDropDown], {
-    templateString: template,
-    hasDownArrow: true,
-    cssStateNodes: {
-      "_buttonNode": "dijitDownArrowButton"
-    },
-    value: new Date('1970-01-01 00:00:00.000'),
-    pattern: '[0-9]{4}-[0-9]{2}-[0-9]{2}[T ][0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]{3})?Z?',
-
-
-    format: function(value, constraints) {
-      return value.toISOString();
-    },
-    parse: function(value, constraints) {
-      return new Date(value);
-    },
-
-
-    buildRendering: function(){
-      this.inherited(arguments);
-
-      if(!this.hasDownArrow){
-        this._buttonNode.style.display = "none";
-      }
-
-      if(!this.hasDownArrow){
-        this._buttonNode = this.domNode;
-        this.baseClass += " dijitComboBoxOpenOnClick";
-      }
-    },
-
-    _setValueAttr: function(value, priorityChange, formattedValue){
-      this.inherited(arguments, [value, priorityChange, formattedValue]);
-      if(this.dropDown){
-        this.dropDown.set('value', value, false);
-      }
-    },
-
-    openDropDown: function(callback){
-      if(this.dropDown){
-        this.dropDown.destroy();
-      }
-      var textBox = this,
-      value = this.get("value");
-      this.dropDown = new this.constructor.DateTimeSelector({
-        onChange: function(value){
-          textBox.set('value', value.toISOString(), true);
-//          popup.close(textBox.dropDown);
-        },
-        value: value,
-        id: this.id + "_popup",
-        dir: textBox.dir,
-        lang: textBox.lang,
-        textDir: textBox.textDir,
-      });
-
-      this.inherited(arguments);
-    }
-  });
-
-  DateTimeDropdown.DateTimeSelector = declare("DateTimeSelector", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, _Container], {
+  var DateTimeSelector = declare("DateTimeSelector", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, _Container], {
     templateString: '' +
       '<div class="${baseClass}" style="background: white; padding: 8px; border: solid 1px #b5bcc7; border-collapse: separate; -moz-border-radius: 4px; border-radius: 4px;">' +
       '  <div data-dojo-type="dijit/Calendar" data-dojo-attach-point="calendar" data-dojo-attach-event="onChange:setDate"></div>' +
-      '  <div style="height: 100px; overflow: auto; margin-top: 8px; border: solid 1px #b5bcc7; border-collapse: separate; -moz-border-radius: 4px; border-radius: 4px;" data-dojo-type="dijit/_TimePicker" data-dojo-attach-point="time" data-dojo-attach-event="onChange:setTime"></div>' +
+      '  <div style="height: 100px; overflow: auto; margin-top: 8px; border: solid 1px #b5bcc7; border-collapse: separate; -moz-border-radius: 4px; border-radius: 4px;" data-dojo-type="dijit/_TimePicker" data-dojo-attach-point="time" data-dojo-attach-event="onChange:setTime" data-dojo-props="constraints: {selector: \'time\'}"></div>' +
       '</div>',
 
     value: new Date(),
 
+    constraints: {selector: 'time', timePattern:'HH:mm:ss', clickableIncrement:'T00:15:00', visibleIncrement:'T00:15:00', visibleRange:'T01:00:00'},
+
     setDateTime: function (date, time) {
       var self = this;
+      if (self.widgetUpdate) return;
       self.set("value", new Date(date.getFullYear(), date.getMonth(), date.getDate(), time.getHours(), time.getMinutes(), time.getSeconds(), time.getMilliseconds()));
     },
 
@@ -112,12 +54,46 @@ define([
 
     _setValueAttr: function (value) {
       var self = this;
+      var old = self.value;
+
+      if (value == null || value == undefined) {
+        value = new Date();
+      }
+
       self._set("value", value);
-      self.onChange(value);
+
+      self.widgetUpdate = true;
+      self.calendar.set("value", value);
+      self.calendar.set("currentFocus", value);
+      self.time.set("value", value);
+      self.time.set("currentFocus", value);
+      self.widgetUpdate = false;
+
+      if ((value && value.toISOString()) != (old && old.toISOString())) {
+        self.onChange(value);
+      }
     },
 
     onChange: function () {}
   });
 
-  return DateTimeDropdown;
+  return declare("DateTimeDropdown", [_DateTimeTextBox], {
+    _selector: undefined,
+    popupClass: DateTimeSelector,
+
+    /* Hack to get ISO date format */
+    constructor: function(){
+     this._set('pattern', this.constructor.prototype.pattern);
+    },
+    pattern: '[0-9]{4}-[0-9]{2}-[0-9]{2}[T ][0-9]{2}:[0-9]{2}:[0-9]{2}\(\.[0-9]{3}\)?Z?',
+    format: function(/*Date*/ value, /*locale.__FormatOptions*/ constraints){
+      if(!value){ return ''; }
+      return value.toISOString().slice(0, -1).replace("T", " ");
+    },
+
+    "parse": function(value, constraints){
+      if(!value){ return null; }
+      return new Date(value.replace(" ", "T"));
+    }
+  });
 });
