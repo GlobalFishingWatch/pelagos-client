43e3970029bd58a1f2bd210bbe5cf2fbd10091bb
Egil <egil.moller@freecode.no>
2016-02-25 16:41:50 +0100

    Show flags in filter list

--- js/app/Visualization/UI/FilterEditorBase.js
+++ js/app/Visualization/UI/FilterEditorBase.js
@@ -1,10 +1,12 @@
 define([
   "dojo/_base/declare",
   "./Widgets/TemplatedDialog",
+  "./TemplatedContainer",
   "dijit/form/Button"
 ], function(
   declare,
-  Dialog
+  Dialog,
+  TemplatedContainer
 ){
   return declare("FilterEditorBase", [Dialog], {
     animation: null,
@@ -54,6 +56,54 @@ define([
         endData[self.sourcename] = Number.POSITIVE_INFINITY;
         selection.addDataRange(startData, endData);
       }
-    }
+    },
+
+    Display: declare("Display", [TemplatedContainer], {
+      templateString: '' +
+        '<div class="${baseClass}" style="display: inline-block;">' +
+        '  <div class="${baseClass}Container" data-dojo-attach-point="containerNode"></div>' +
+        '</div>',
+      noFilterTitle: 'All',
+      animation: null,
+      sourcename: null,
+      startup: function () {
+        var self = this;
+        self.inherited(arguments);
+        var selection = self.animation.data_view.selections.selections.active_category;
+        selection.events.on({update: self.rangeUpdated.bind(self)});
+        self.rangeUpdated();
+      },
+      getItemList: function () {
+        var self = this;
+        var selection = self.animation.data_view.selections.selections.active_category;
+        var source = self.animation.data_view.source.header.colsByName[self.sourcename];
+        var range = selection.data[self.sourcename];
+
+        var title = 'All';
+        if (range.length != 2 || range[0] != Number.NEGATIVE_INFINITY || range[1] != Number.POSITIVE_INFINITY) {
+          var choicesById = {};
+          for (var name in source.choices) {
+            choicesById[source.choices[name]] = name;
+          }
+
+          var res = [];
+          for (var i = 0; i < range.length; i+=2) {
+            res.push({name: choicesById[range[i]], id:range[i]});
+          }
+          return res;
+        }
+        return [];
+      },
+      rangeUpdated: function () {
+        var self = this;
+
+        var list = self.getItemList();
+        var title = self.noFilterTitle;;
+        if (list.length > 0) {
+          title = list.map(function (item) { return item.name; }).join(", ");
+        }
+        self.containerNode.innerHTML = title;
+      }
+    })
   });
 });
