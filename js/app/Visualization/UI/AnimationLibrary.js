7667a9d21913b5fe0fbbdb5c03fe5614b63e372f
Egil <egil.moller@freecode.no>
2016-02-29 16:37:06 +0100

    Templating limitation workaround

--- js/app/Visualization/UI/AnimationLibrary.js
+++ js/app/Visualization/UI/AnimationLibrary.js
@@ -28,7 +28,7 @@ define([
       '  <div data-dojo-type="dijit/layout/ContentPane" data-dojo-props="region:\'top\'" style="border: none; padding: 0; padding-bottom: 10px; margin: 0; overflow: hidden;">' +
       '    <input type="text" class="query" style="width: 100%;" placeholder="Search by name or tag" data-dojo-attach-event="keyup:queryQueyUp"></input>' +
       '    <div class="search-loading">' +
-      '      <img style="width: 20px;" src="${app.dirs.loader}">' +
+      '      <img style="width: 20px;" src="' + app.dirs.loader + '">' +
       '    </div>' +
       '  </div>' +
       '  <div data-dojo-type="dijit/layout/ContentPane" data-dojo-attach-point="sourcesList" data-dojo-props="region:\'center\'" style="border: none; padding: 0; margin: 0;" class="sourcesList"></div>' +
