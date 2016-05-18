deda6dc0d173af1ac247dc361d6273c63f516b83
Egil <egil.moller@freecode.no>
2016-05-18 14:15:09 +0200

    Made an example with only data

--- js/app/main-visualization-only.js
+++ js/app/main-visualization-only.js
@@ -1,22 +1,16 @@
-/* Commented out sections loads the UI */
-
 define([
   "shims/async/main",
   "shims/jQuery/main",
-  "app/Visualization/Visualization",
-  "app/Visualization/UI/UIManager"
+  "app/Visualization/Visualization"
 ], function (
   async,
   $,
-  Visualization,
-  UIManager
+  Visualization
 ) {
   $(document).ready(function () {
     visualization = new Visualization('#visualization');
     visualization.init(function () {
-      visualization.ui = new UIManager(visualization);
       async.series([
-        visualization.ui.init.bind(visualization.ui),
         visualization.loadConfiguration.bind(visualization),
         visualization.load.bind(visualization, undefined)
       ]);
