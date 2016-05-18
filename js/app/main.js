cd2d5965d43fcf3aae13bc4c1a44b0308f113d33
Egil <egil.moller@freecode.no>
2016-05-18 12:23:30 +0200

    Don't load the UI

--- js/app/main.js
+++ js/app/main.js
@@ -1,20 +1,22 @@
+/* Commented out sections loads the UI */
+
 define([
   "shims/async/main",
   "shims/jQuery/main",
-  "app/Visualization/Visualization",
-  "app/Visualization/UI/UIManager"
+  "app/Visualization/Visualization" /* ,
+  "app/Visualization/UI/UIManager" */
 ], function (
   async,
   $,
-  Visualization,
-  UIManager
+  Visualization /*,
+  UIManager */
 ) {
   $(document).ready(function () {
     visualization = new Visualization('#visualization');
     visualization.init(function () {
-      visualization.ui = new UIManager(visualization);
+//      visualization.ui = new UIManager(visualization);
       async.series([
-        visualization.ui.init.bind(visualization.ui),
+//        visualization.ui.init.bind(visualization.ui),
         visualization.loadConfiguration.bind(visualization),
         visualization.load.bind(visualization, undefined)
       ]);
