1de4904b45ea096c39c4d19e20d24b810023ad42
Egil <egil.moller@freecode.no>
2016-05-18 10:01:10 +0200

    Removed app.dirs

--- js/app/main.js
+++ js/app/main.js
@@ -4,53 +4,6 @@ define([
   require
 ) {
   app = {};
-
-  app.paths = app.paths || {};
-
-  var mainUrl = require.toUrl(".");
-  app.paths.script = mainUrl.split("/").slice(0, -1);
-  app.paths.root = app.paths.script.slice(0, -1);
-
-  app.paths.build = app.paths.root.concat(['js-build']);
-  app.paths.lib = app.paths.script.concat(['libs']);
-  app.paths.img = app.paths.root.concat(['img']);
-  if (app.useBuild) {
-    app.paths.shim = app.paths.build;
-    app.paths.app = app.paths.build.concat(['app']);
-  } else {
-    app.paths.shim = app.paths.script.concat("shims");
-    app.paths.app = app.paths.script.concat(['app']);
-  }
-
-  if (navigator.appName == 'Microsoft Internet Explorer' || /MSIE/i.test(navigator.userAgent) || /Edge/i.test(navigator.userAgent)) {
-    app.paths.loader = app.paths.img.concat(["loader", "spinner.min.gif"]);
-  } else {
-    app.paths.loader = app.paths.img.concat(["loader", "spinner.min.svg"]);
-  }
-
-  app.dirs = app.dirs || {};
-  for (var name in app.paths) {
-    app.dirs[name] = app.paths[name].join("/");
-  }
-
-  var stylesheets = [];
-
-  if (app.useBuild) {
-    stylesheets = stylesheets.concat([
-      "$(build)s/deps.css",
-      "$(lib)s/dojo-theme-flat/CSS/dojo/flat.css",
-      {url: "$(script)s/../style.less", rel:"stylesheet/less"}
-    ]);
-  } else {
-    stylesheets = stylesheets.concat([
-      "libs/font-awesome/css/font-awesome.min.css",
-      "libs/dojo-theme-flat/CSS/dojo/flat.css",
-      "libs/dojox/layout/resources/FloatingPane.css",
-      "libs/dojox/layout/resources/ResizeHandle.css",
-      {url: "app/style.less", rel:"stylesheet/less"},
-    ]);
-  }
-
   require([
     "shims/GoogleMaps/main",
     "shims/async/main",
@@ -78,6 +31,14 @@ define([
      Stats,
      Styles
   ) {
+    var stylesheets = [
+      "libs/font-awesome/css/font-awesome.min.css",
+      "libs/dojo-theme-flat/CSS/dojo/flat.css",
+      "libs/dojox/layout/resources/FloatingPane.css",
+      "libs/dojox/layout/resources/ResizeHandle.css",
+      {url: "app/style.less", rel:"stylesheet/less"},
+    ];
+
     stylesheets.map(Styles.add);
 
     less.registerStylesheets($("link[rel='stylesheet/less']"));
