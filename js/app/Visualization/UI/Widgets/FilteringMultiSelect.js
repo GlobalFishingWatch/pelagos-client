25d2db2c50de96bda5f0938cef121e0dd495d007
Egil <egil.moller@freecode.no>
2016-08-23 10:56:48 +0200

    Better focus handling and support for fancy labels

--- js/app/Visualization/UI/Widgets/FilteringMultiSelect.js
+++ js/app/Visualization/UI/Widgets/FilteringMultiSelect.js
@@ -36,6 +36,8 @@ define([
         id: self.get("id") + "_select",
         name: self.get('name'),
         store: self.get('store'),
+        labelAttr: self.get('labelAttr'),
+        labelType: self.get('labelType'),
         searchAttr: self.get('searchAttr'),
         required: false,
         onInput: function(event) {
@@ -51,7 +53,8 @@ define([
         }
       });
 
-      $(this.domNode).append(self._select.domNode);
+      self.focusNode = self._select.focusNode;
+      $(self.domNode).append(self._select.domNode);
       self.selectedItemsNode = $(self.domNode).find(".dijitInputContainer");
  
       self._select.startup();
@@ -73,6 +76,18 @@ define([
     _setStoreAttr: function(value) {
       var self = this;
       if (self._select) self._select.set("store", value);
+      this.inherited(arguments);
+    },
+
+    _setLabelAttrAttr: function(value) {
+      var self = this;
+      if (self._select) self._select.set("labelAttr", value);
+      this.inherited(arguments);
+    },
+
+    _setLabelTypeAttr: function(value) {
+      var self = this;
+      if (self._select) self._select.set("labelType", value);
       this.inherited(arguments);
     },
 
@@ -128,10 +143,15 @@ define([
     multiSelect: null,
     templateString: "<span><span data-dojo-attach-point='text'></span><button data-dojo-attach-event='click:onClick' style='display: inline-block;'>X</button> </span>",
     _setItemAttr: function (document) {
-      this.inherited(arguments);
-      //var searchAttr = this.getParent().get("searchAttr")
-        searchAttr = 'name';
-      $(this.text).text(this.item[searchAttr]);
+      var self = this;
+      self.inherited(arguments);
+      var attr = self.multiSelect.get("labelAttr") || self.multiSelect.get("searchAttr") || "name";
+      var labelType = self.multiSelect.get("labelType") || "text"
+      if (labelType == "text") {
+        $(self.text).text(self.item[attr]);
+      } else {
+        $(self.text).html(self.item[attr]);
+      }
     },
     onClick: function (e) {
       var self = this;
