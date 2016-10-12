9aee3192cbd6deed1de5f2068d38d398aa428bbb
Egil <egil.moller@freecode.no>
2016-10-12 14:10:30 +0200

    Bug workaround for magic description rewriting

--- js/app/Data/CartoDBFormat.js
+++ js/app/Data/CartoDBFormat.js
@@ -4,6 +4,7 @@ define([
   "app/Data/Format",
   "app/Data/EmptyFormat",
   "app/Data/Ajax",
+  "shims/jQuery/main",
   "shims/async/main"
 ], function(
   Class,
@@ -11,48 +12,62 @@ define([
   Format,
   EmptyFormat,
   Ajax,
+  $,
   async
 ) {
   var CartoDBFormat = Class(EmptyFormat, {
     name: "CartoDBFormat",
 
-    getSelectionInfo: function(selection, cb) {
+    getMetadata: function(cb) {
       var self = this;
 
-      if (selection !== undefined) return cb("Not implemented", null);
-
-      var info;
+      var metadata;
       var title;
       async.series([
         function (cb) {
           Ajax.get(self.url, {}, function (err, data) {
             if (err) return cb(err);
             title = data.title;
-            info = data.description || "";
+            metadata = data.description || "";
             cb();
           });
         },
         function (cb) {
-          try {
-            info = JSON.parse(info);
-            cb();
-          } catch (e) {
-            if (info.indexOf('://') != -1 && info.indexOf('<') == -1) {
-              Ajax.get(info, {}, function (err, data) {
-                if (err) return cb(err);
-                info = JSON.parse(data);
-                cb();
-              });
-            } else {
-              info = {'description': info};
+          info_link = $(metadata).find("a:contains('info')");
+          if (info_link.length > 0) {
+            Ajax.get(info_link.attr("href"), {}, function (err, data) {
+              if (err) return cb(err);
+              metadata = data;
               cb();
-            }
+            });
+          } else {
+              metadata = {info: {'description': metadata}};
+            cb();
           }
         }
       ], function (err) {
         if (err) return cb(err);
-        if (title) info.title = title;
-        cb(null, info);
+        if (title) metadata.info.title = title;
+        cb(null, metadata);
+      });
+    },
+
+    getSelectionInfo: function(selection, cb) {
+      var self = this;
+
+      if (selection !== undefined) return cb("Not implemented", null);
+      self.getMetadata(function (err, metadata) {
+        if (err) return cb(err);
+        cb(null, metadata.info);
+      });
+    },
+
+    getHeader: function(cb) {
+      var self = this;
+
+      self.getMetadata(function (err, metadata) {
+        if (err) return cb(err);
+        cb(null, metadata.header);
       });
     },
 
